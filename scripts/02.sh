b390d0f8492e2766c9ec11fc38eed29c5f4058fe
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2020-01-10 10:48:23 +0200

    Added scripts

--- scripts/02.sh
+++ scripts/02.sh
@@ -4,7 +4,8 @@
 if [ "$1" ]
 then
 	read -p "Deleting user $1. Press any key to continue."
-    echo "$1 is input"
+	pkill -u $1
+	sudo deluser $1
 
 else
     read -p "Usage: bash 02.sh [username]. You didn't pick a user. Press enter for a list."
